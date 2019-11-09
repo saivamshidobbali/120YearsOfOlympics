49e394680042b482e08b56ef5019004e36286081
Kiranmayee <kiranmayee.dobbali@gmail.com>
2019-11-08 17:51:23 -0700

    modified table

--- Olympics/js/table2.js
+++ Olympics/js/table2.js
@@ -3,7 +3,7 @@ class Table2{
 constructor(teamData){
 this.tableElements= teamData;
 this.cell = {
-    "width": 150,
+    "width": 260,
     "height": 50,
     "buffer": 15
 };
@@ -14,10 +14,10 @@ this.bar = {
 this.gameScale = d3.scaleLinear()
     .range([0,this.cell.width]);
 
-
+this.ascending=true;
 }
 
-createTable(){
+createTable(activeYear="2016"){
 let that = this;
 
 console.log(that.tableElements);
@@ -39,7 +39,7 @@ let tablehead = tablerow.selectAll("th")
                           console.log([d]);
                           return [d];   })
                         .join("th")
-                        .text(function(d){
+                        .text(function(d,i){
                           //if(d.value.key.value.type == "game"){
                           //return d.key;
                         //}
@@ -47,18 +47,19 @@ let tablehead = tablerow.selectAll("th")
                         //  return d.key;
                         //}
                      //
-                     return d.key;
+                     console.log(d.values[i].value["Country Name"]);
+                     return d.values[i].value["Country Name"];
                    });
 
 
 let td = tablerow.selectAll("td")
                 .data(function (d, i) {
                     let list = []
-                    console.log(d.values);
+                  //  console.log(d.values);
                    let a = d.values;
                    for (let ele in a){
                      console.log("ele",d.values[ele].key);
-                     if(d.values[ele].key=="1980"){
+                     if(d.values[ele].key=="2016"){
                        list.push(d.values[ele].value['Total Gold'])
                        list.push(d.values[ele].value['Total Bronze'])
                        list.push(d.values[ele].value['Total Silver'])
@@ -71,37 +72,43 @@ let td = tablerow.selectAll("td")
                 })
                 td = td.join('td');
 
-                let bars = td.selectAll("svg")
-                    .data(function (d) {
-                        return [d];
-                    }).join('svg')
-                    .attr("width", this.cell.width)
-                    .attr("height", this.cell.height);
+let bars = td.selectAll("svg")
+            .data(function (d) {
+                return [d];
+                  }).join('svg')
+              .attr("width", this.cell.width)
+              .attr("height", this.cell.height);
 
-              let rect = bars.selectAll("rect")
-                  .data(function (d) {
-                      return [d];
-                  }).join('rect')
-                  .attr("width", function (d) {
-console.log(d);
+let rect = bars.selectAll("rect")
+                .data(function (d) {
+                    return [d];
+                }).join('rect')
+                .attr("width", function (d) {
+            //console.log(d);
                       return that.gameScale(d);
                   })
                   .attr("height", 40)
                   //.attr("fill", function (d) {
                   //    return aggregateColorScale(d.value)
                   //})
-        let barstext = bars.selectAll("text")
-            .data(function (d) {
-                return [d];
-            }).join('text')
-            .attr("x", function (d) {
-                return that.gameScale(d);
-            })
-            .attr("y", this.cell.height / 2 + 5)
-            .attr("class", "label")
-            .text(function (d) {
-                return d;
-            })
+let barstext = bars.selectAll("text")
+                    .data(function (d) {
+                        return [d];
+                    }).join('text')
+                    .attr("x", function (d) {
+                        return that.gameScale(d)+5;
+                    })
+                    .attr("y", this.cell.height / 2 + 5)
+                    .attr("class", "label")
+                    .text(function (d) {
+                        return d;
+                    })
+
+
+}
+
+updateTable(activeYear="2016"){
+
 
 
 }

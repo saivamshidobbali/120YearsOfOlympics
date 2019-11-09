5a2d2f3241fa5119a37be5951f76ce133a34ea06
saivamshidobbali <u1266122@utah.edu>
2019-11-08 19:34:48 -0700

    fixing git

---
+++ Olympics/js/script.js
@@ -0,0 +1,111 @@
+d3.json('data/gdp.json').then( gdp_data=> {
+this.active_year = "1980";
+
+
+ d3.csv('data/truncated_latest.csv').then(matchesCSV=>{
+  let MedalsData = d3.nest()
+                  .key(d=> d.NOC)
+                  .key(d=>d.Year)
+                  .entries(matchesCSV)
+
+ d3.csv('data/athlete_events_modified.csv').then(participantsCSV=>{
+
+  let participantsInfo = d3.nest()
+               .key(d=> d.NOC)
+               .key(d=>d.Year)
+               .rollup()
+               .entries(participantsCSV)  
+
+   const gap_plot = new GapPlot(MedalsData, gdp_data, participantsInfo, updateyear);
+   gap_plot.drawPlot();
+
+ })
+
+
+let teamData = d3.nest()
+                 .key(d=> d.NOC)
+                 .key(d=>d.Year)
+                 .rollup(leaves => {
+
+let country_name = leaves[0]['Team'];
+
+len= leaves.length;
+         let total_medals = len;
+         var total_gold=0;
+         var total_bronze=0;
+         var total_silver=0;
+         var t_gold=0;
+         var t_bronze=0;
+         var t_silver=0;
+         let games2 = [];
+         var sports = []
+
+         for(var i=0;i<len;i++){
+
+         h(i);
+         }
+         function h(i){
+
+                 if(leaves[i]['Medal']=="Gold"){
+                   total_gold+=1;
+                 }
+                 if(leaves[i]['Medal']=="Bronze"){
+                   total_bronze+=1;
+                 }
+                 if(leaves[i]['Medal']=="Silver"){
+                   total_silver+=1;
+         }
+         let game = {};
+         if(sports.includes(leaves[i]['Sport'])==false)
+         {
+           sports.push(leaves[i]['Sport'])
+           game.key = leaves[i]["Sport"];
+           if(leaves[i]['Medal']=="Gold"){
+             t_gold+=1;
+           }
+           if(leaves[i]['Medal']=="Bronze"){
+             t_bronze+=1;
+           }
+           if(leaves[i]['Medal']=="Silver"){
+             t_silver+=1;
+   }
+       game.value = {
+         "Total Gold": t_gold,
+         "Total Silver":t_silver,
+         "Total Bronze":t_bronze,
+         "type":"game"
+       }
+       games2.push(game);
+
+         }
+
+      }
+
+
+       let obj = {
+         "Country Name":country_name,
+         "Total Medals":total_medals,
+         "Total Gold": total_gold,
+         "Total Silver":total_silver,
+         "Total Bronze":total_bronze,
+         "Sports":games2,
+         "type":"aggregate"
+       }
+
+
+       return obj;
+     }).entries(matchesCSV);
+
+
+
+    var table = new Table2(teamData);
+    table.createTable(this.active_year);
+
+function updateyear(active_year) {
+       this.active_year = active_year;
+       table.createTable(this.active_year);
+}
+
+});
+});
+
---
+++ Olympics/js/table.js
@@ -0,0 +1,103 @@
+class Table2{
+
+constructor(teamData){
+this.tableElements= teamData;
+this.cell = {
+    "width": 150,
+    "height": 50,
+    "buffer": 15
+};
+
+this.bar = {
+    "height": 20
+};
+this.gameScale = d3.scaleLinear()
+    .range([0,this.cell.width]);
+
+
+}
+
+createTable(activeYear){
+let that = this;
+
+//let maxTotalGames= d3.max(that.tableElements, function (d) {
+  // return d.values;
+//})
+
+that.gameScale.domain([0,500]);
+
+
+let tablerow = d3.select("tbody").selectAll("tr")
+     .data(that.tableElements)
+     .join('tr');
+
+//tablerow.text(function(d){
+//return d.key;
+let tablehead = tablerow.selectAll("th")
+                        .data(function (d) {
+                          return [d];   })
+                        .join("th")
+                        .text(function(d){
+                          //if(d.value.key.value.type == "game"){
+                          //return d.key;
+                        //}
+                        //else {
+                        //  return d.key;
+                        //}
+                     //
+                     return d.key;
+                   });
+
+
+let td = tablerow.selectAll("td")
+                .data(function (d, i) {
+                    let list = []
+                   let a = d.values;
+                   for (let ele in a){
+                     if(d.values[ele].key==activeYear){
+                       list.push(d.values[ele].value['Total Gold'])
+                       list.push(d.values[ele].value['Total Bronze'])
+                       list.push(d.values[ele].value['Total Silver'])
+                       list.push(d.values[ele].value['Total Medals'])
+                     }
+                   }
+
+
+                    return list
+                })
+                td = td.join('td');
+
+                let bars = td.selectAll("svg")
+                    .data(function (d) {
+                        return [d];
+                    }).join('svg')
+                    .attr("width", this.cell.width)
+                    .attr("height", this.cell.height);
+
+              let rect = bars.selectAll("rect")
+                  .data(function (d) {
+                      return [d];
+                  }).join('rect')
+                  .attr("width", function (d) {
+                      return that.gameScale(d);
+                  })
+                  .attr("height", 40)
+                  //.attr("fill", function (d) {
+                  //    return aggregateColorScale(d.value)
+                  //})
+        let barstext = bars.selectAll("text")
+            .data(function (d) {
+                return [d];
+            }).join('text')
+            .attr("x", function (d) {
+                return that.gameScale(d);
+            })
+            .attr("y", this.cell.height / 2 + 5)
+            .text(function (d) {
+                return d;
+            })
+
+
+}
+
+}

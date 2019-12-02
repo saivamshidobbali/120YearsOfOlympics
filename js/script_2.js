64889efc84781a431cf3fcabe5e34bb6a8370994
saivamshidobbali <u1266122@utah.edu>
2019-12-01 23:24:07 -0700

    final

--- js/script_2.js
+++ js/script_2.js
@@ -54,20 +54,6 @@ console.log(teamData);
 
 
    // var line = new MultiLine();
-    //line.line();
-       "use strict";
-exportToJsonFile(teamData);
-    function exportToJsonFile(teamData) {
-        let dataStr = JSON.stringify(teamData);
-        let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
-
-        let exportFileDefaultName = 'data.json';
-
-        let linkElement = document.createElement('a');
-        linkElement.setAttribute('href', dataUri);
-        linkElement.setAttribute('download', exportFileDefaultName);
-        linkElement.click();
-    }
 
 
 });
